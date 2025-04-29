c2ad17834d1750398466ba47d2f9f70d462c1d60
garciabryanf <garciabryanfarrel_2327250026@mhs.mdp.ac.id>
2025-04-29 13:11:28 +0700

    Update main.dart

--- bryanfarrel_apps/lib/main.dart
+++ bryanfarrel_apps/lib/main.dart
@@ -4,34 +4,46 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
-
 void main() async {
+  // Ensure Flutter's binding is initialized
+  WidgetsFlutterBinding.ensureInitialized();
+  
+  // Initialize Firebase before running the app
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+  
+  // Run your app after Firebase has been initialized
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Fasum',
-        theme: ThemeData(
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-          useMaterial3: true,
-        ),
-        home: StreamBuilder(
-            stream: FirebaseAuth.instance.authStateChanges(),
-            builder: (context, snapshot) {
-              if (snapshot.hasData) {
-                return const HomeScreen();
-              } else {
-                return const SignInScreen();
-              }
-            }));
+      title: 'Fasum',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: StreamBuilder<User?>(
+        // Use FirebaseAuth instance and its stream for authentication state
+        stream: FirebaseAuth.instance.authStateChanges(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            // Show a loading indicator while waiting for authentication
+            return const Center(child: CircularProgressIndicator());
+          } else if (snapshot.hasData) {
+            // If there's data, the user is authenticated, show the HomeScreen
+            return const HomeScreen();
+          } else {
+            // If no data, the user is not authenticated, show the SignInScreen
+            return const SignInScreen();
+          }
+        },
+      ),
+    );
   }
 }
